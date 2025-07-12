b2576b55cdebb0390795ec3bf4abfe51842d775a
quysmile <quyt26606@gmail.com>
2025-07-13 04:58:59 +0700

    Refactor StudentMenu.java to reduce method complexity

--- src/ui/StudentMenu.java
+++ src/ui/StudentMenu.java
@@ -7,70 +7,88 @@ import service.ManageStudentList;
 
 public class StudentMenu {
     private ManageStudentList studentList;
-    // private int checkChoice = 0;
-    // private int secondChoice = 0;
+    private final Scanner scanner = new Scanner(System.in);
 
     public StudentMenu(ManageStudentList studentList) {
         this.studentList = studentList;
     }
 
     public void run() {
+        printMainMenu();
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter your choice: ");
+        int yourChoice = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+
+        switch (yourChoice) {
+            case 1:
+                handleAddStudents();
+                break;
+            case 2:
+                handleDeleteStudent();
+                break;
+            case 3:
+                handleSearchStudent();
+                break;
+            case 4:
+                handleDisplayAll();
+                break;
+            case 5:
+                System.out.println("Exiting...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Number!");
+        }
+        secondMenu();
+    }
+
+    private void printMainMenu() {
         System.out.println("----------* MANAGE STUDENTS SERVICE *-----------");
         System.out.println("1. Add student");
         System.out.println("2. Delete student");
         System.out.println("3. Search student");
         System.out.println("4. Display all");
         System.out.println("5. Exit");
-        System.out.println("\nEnter your choice: ");
-        int yourChoice = scanner.nextInt();
+    }
 
-        switch (yourChoice) {
-            case 1:
-                System.out.println("Enter number of Student want to add: ");
-                int numberOfStudent = scanner.nextInt();
-                for (int i = 0; i < numberOfStudent; i++) {
-                    System.out.print("Enter Student ID: ");
-                    int id = scanner.nextInt();
-                    scanner.nextLine();
+    private void handleAddStudents() {
+        System.out.print("Enter number of students to add: ");
+        int numberOfStudent = scanner.nextInt();
+        scanner.nextLine();
 
-                    System.out.print("Enter Full Name: ");
-                    String fullName = scanner.nextLine();
+        for (int i = 0; i < numberOfStudent; i++) {
+            System.out.print("Enter Student ID: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
 
-                    System.out.print("Enter GPA: ");
-                    double gpa = scanner.nextDouble();
+            System.out.print("Enter Full Name: ");
+            String fullName = scanner.nextLine();
 
-                    Student student = new Student(id, fullName, gpa);
-                    studentList.addAStudent(student);
-                }
-                secondMenu();
-                break;
-            case 2:
-                System.out.print("Enter ID Student want to remove: ");
-                int IdStudentRemove = scanner.nextInt();
-                System.out.println("hehe");
-                studentList.deleteAStudent(IdStudentRemove);
-                secondMenu();
-                break;
-            case 3:
-                System.out.print("Enter name Student want to search: ");
-                scanner.nextLine();
-                String nameToSearch = scanner.nextLine();
-                studentList.searchForStudent(nameToSearch);
-                secondMenu();
-                break;
-            case 4:
-                studentList.printAllStudent();
-                secondMenu();
-                break;
-            case 5:
-                break;
-            default:
-                System.out.println("Invalid Number!");
-                break;
+            System.out.print("Enter GPA: ");
+            double gpa = scanner.nextDouble();
+            scanner.nextLine();
+
+            Student student = new Student(id, fullName, gpa);
+            studentList.addAStudent(student);
         }
-        scanner.close();
+    }
+
+    private void handleDeleteStudent() {
+        System.out.print("Enter ID of student to remove: ");
+        int id = scanner.nextInt();
+        studentList.deleteAStudent(id);
+    }
+
+    private void handleSearchStudent() {
+        System.out.print("Enter name of student to search: ");
+        String name = scanner.nextLine();
+        System.out.print("\n");
+        studentList.searchForStudent(name);
+    }
+
+    private void handleDisplayAll() {
+        studentList.printAllStudent();
     }
 
     public void secondMenu() {
